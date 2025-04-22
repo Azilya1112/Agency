30fe377a92c5177ff933a27d1073f4180611f4c5
Azilia Adylgazieva <Azilia.Adylgazieva.o3k@fh-zwickau.de>
2025-04-22 03:50:22 +0200

    feat: add complete frontend implementation

--- src/main/java/com/example/agency/config/MvcConfig.java
+++ src/main/java/com/example/agency/config/MvcConfig.java
@@ -4,16 +4,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class MvcConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/assets/");
-        registry.addResourceHandler("/assets_admin/**").addResourceLocations("classpath:/assets_admin/");
-        registry.addResourceHandler("/examples/**").addResourceLocations("classpath:/examples/");
-        registry.addResourceHandler("/forms/**").addResourceLocations("classpath:/forms/");
-        registry.addResourceHandler("/templates/**").addResourceLocations("classpath:/templates/");
-
-    }
-}
+//@Configuration
+//public class MvcConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/assets/");
+//        registry.addResourceHandler("/assets_admin/**").addResourceLocations("classpath:/assets_admin/");
+//        registry.addResourceHandler("/examples/**").addResourceLocations("classpath:/examples/");
+//        registry.addResourceHandler("/forms/**").addResourceLocations("classpath:/forms/");
+//        registry.addResourceHandler("/templates/**").addResourceLocations("classpath:/templates/");
+//
+//    }
+//}
